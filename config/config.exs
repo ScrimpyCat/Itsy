03e8f0dd475a2ce16a5152d389169cae44c84980
Stefan Johnson <sj@scrimpycat.io>
2018-06-29 00:19:28 +1000

    Remove comments

--- config/config.exs
+++ config/config.exs
@@ -5,15 +5,5 @@ use Mix.Config
 if Mix.env == :dev do
     import_config "simple_markdown_rules.exs"
 
-    # config :simple_markdown_extension_highlight_js,
-    #     source: Enum.at(Path.wildcard(Path.join(Mix.Project.deps_path(), "ex_doc/formatters/html/dist/*.js")), 0, "")
-
-    # config :ex_doc_simple_markdown, [
-    #     rules: fn rules ->
-    #         :ok = SimpleMarkdownExtensionHighlightJS.setup
-    #         SimpleMarkdownExtensionBlueprint.add_rule(rules)
-    #     end
-    # ]
-
     config :ex_doc, :markdown_processor, ExDocSimpleMarkdown
 end
