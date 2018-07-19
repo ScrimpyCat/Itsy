b29446af638dd13422b01ae2d6ef18667c25afe4
Stefan Johnson <sj@scrimpycat.io>
2018-07-19 15:26:56 +1000

    Add precision and rounding options, and update specs

--- lib/itsy/float.ex
+++ lib/itsy/float.ex
@@ -4,6 +4,14 @@ defmodule Itsy.Float do
 
     alias Itsy.Bit
 
+    @type rounding :: :down | :up | :even
+    @type sign_size :: non_neg_integer
+    @type exponent_size :: non_neg_integer
+    @type mantissa_size :: non_neg_integer
+    @type encoding :: { sign_size, exponent_size, mantissa_size }
+    @type precision :: encoding | 16 | 32 | 64 | 128 | 256
+    @type options :: [rounding: rounding, precision: precision]
+
     @spec sign(float) :: integer
     def sign(v) do
         <<s :: size(1), _ :: bitstring>> = <<v :: float>>
@@ -22,18 +30,35 @@ defmodule Itsy.Float do
         m
     end
 
-    @spec new(integer, integer) :: float
-    def new(value, exponent \\ 0), do: integer_to_float(value, exponent * -1)
+    @spec format_options(options) :: [rounding: rounding, precision: encoding]
+    defp format_options(opts) do
+        opts = Keyword.merge([precision: 64, rounding: :even], opts)
+        case opts[:precision] do
+            256 -> Keyword.replace!(opts, :precision, { 1, 19, 236 })
+            128 -> Keyword.replace!(opts, :precision, { 1, 15, 112 })
+            64 -> Keyword.replace!(opts, :precision, { 1, 11, 52 })
+            32 -> Keyword.replace!(opts, :precision, { 1, 8, 23 })
+            16 -> Keyword.replace!(opts, :precision, { 1, 5, 10 })
+            precision when is_tuple(precision) -> opts
+        end
+    end
 
-    @spec whole_to_float(integer) :: float
-    defp whole_to_float(v) do
+    @spec new(integer, integer, options) :: float
+    def new(value, exponent \\ 0, opts \\ []) do
+        opts = format_options(opts)
+        integer_to_float(value, exponent * -1, opts[:rounding], opts[:precision])
+    end
+
+    @spec whole_to_float(integer, mantissa_size) :: float
+    defp whole_to_float(v, mp) do
         e = Bit.highest_set(v) |> Bit.mask_lower_power_of_2
         m = e &&& v
         e = Bit.count(e)
 
-        { e, m <<< (52 - e) }
+        { e, m <<< (mp - e) }
     end
 
+    @spec rounding(integer, integer, integer, rounding) :: 0 | 1
     defp rounding(_, _, _, :down), do: 0
     defp rounding(_, _, _, :up), do: 1
     defp rounding(v, precision, m, :even) do
@@ -44,45 +69,83 @@ defmodule Itsy.Float do
         end
     end
 
-    defp fraction_to_mantissa(v, e, precision, m \\ 0, index \\ 0)
-    defp fraction_to_mantissa(0, _, _, m, _), do: m
-    defp fraction_to_mantissa(v, _, precision, m, 52), do: m + rounding(v, precision, m, :even)
-    defp fraction_to_mantissa(v, e, precision, m, index) do
+    @spec fraction_to_mantissa(integer, integer, integer, rounding, mantissa_size, integer, integer) :: integer
+    defp fraction_to_mantissa(v, e, precision, rounding, size, m \\ 0, index \\ 0)
+    defp fraction_to_mantissa(0, _, _, _, _, m, _), do: m
+    defp fraction_to_mantissa(v, _, precision, rounding, size, m, size), do: m + rounding(v, precision, m, rounding)
+    defp fraction_to_mantissa(v, e, precision, rounding, size, m, index) do
         v = rem(v, precision) * 2
-        m = m ||| ((boolean_to_integer(v >= precision) <<< (51 - index)) >>> e)
-        fraction_to_mantissa(v, e, precision, m, index + 1)
+        m = m ||| ((boolean_to_integer(v >= precision) <<< ((size - 1) - index)) >>> e)
+        fraction_to_mantissa(v, e, precision, rounding, size, m, index + 1)
     end
 
-    defp fraction_to_exponent(v, precision, e \\ nil, index \\ 0)
-    defp fraction_to_exponent(v, _, e, _) when e != nil, do: { e, v }
-    defp fraction_to_exponent(0, _, _, _), do: { nil, 0 }
-    defp fraction_to_exponent(v, _, _, 1022), do: { 1022, v }
-    defp fraction_to_exponent(v, precision, _, index) do
+    @spec fraction_to_exponent(integer, integer, integer, integer, integer) :: { integer, integer }
+    defp fraction_to_exponent(v, precision, max, e \\ nil, index \\ 0)
+    defp fraction_to_exponent(v, _, _, e, _) when e != nil, do: { e, v }
+    defp fraction_to_exponent(0, _, _, _, _), do: { nil, 0 }
+    defp fraction_to_exponent(v, _, max, _, max), do: { max, v }
+    defp fraction_to_exponent(v, precision, opts, _, index) do
         v = rem(v, precision) * 2
-        fraction_to_exponent(v, precision, if(v >= precision, do: index), index + 1)
+        fraction_to_exponent(v, precision, opts, if(v >= precision, do: index), index + 1)
     end
 
-    defp integer_to_float(v, precision) when precision < 0 do
+    @spec integer_to_float(integer, integer, rounding, encoding) :: float
+    defp integer_to_float(v, precision, rounding, encoding = { sp, ep, mp }) when precision < 0 do
         precision = pow10(abs(precision))
-        { e, m } = integer_to_float(0, abs(v) * precision, precision)
-        <<f :: float>> = <<boolean_to_integer(v < 0) :: size(1), e + 1023 :: size(11), m :: size(52)>>
+        { e, m } = integer_to_float(0, abs(v) * precision, precision, rounding, encoding)
+
+        s = case sp do
+            0 -> 0
+            _ -> boolean_to_integer(v < 0)
+        end
+
+        e = case ep do
+            0 -> 0
+            _ -> min(e, Bit.set(ep - 1)) + 1023
+        end
+
+        m = case mp do
+            0 -> 0
+            size when size <= 52 -> min(m, Bit.set(mp)) <<< (52 - size)
+            size -> min(m, Bit.set(mp)) >>> abs(52 - size)
+        end
+
+        <<f :: float>> = <<s :: 1, e :: 11, m :: 52>>
         f
     end
-    defp integer_to_float(v, precision) do
+    defp integer_to_float(v, precision, rounding, encoding = { sp, ep, mp }) do
         precision = pow10(precision)
-        { e, m } = integer_to_float(abs(v), div(abs(v), precision), precision)
-        <<f :: float>> = <<boolean_to_integer(v < 0) :: size(1), e + 1023 :: size(11), m :: size(52)>>
+        { e, m } = integer_to_float(abs(v), div(abs(v), precision), precision, rounding, encoding)
+
+        s = case sp do
+            0 -> 0
+            _ -> boolean_to_integer(v < 0)
+        end
+
+        e = case ep do
+            0 -> 0
+            _ -> min(e, Bit.set(ep - 1)) + 1023
+        end
+
+        m = case mp do
+            0 -> 0
+            size when size <= 52 -> min(m, Bit.set(mp)) <<< (52 - size)
+            size -> min(m, Bit.set(mp)) >>> abs(52 - size)
+        end
+
+        <<f :: float>> = <<s :: 1, e :: 11, m :: 52>>
         f
     end
 
-    defp integer_to_float(v, 0, precision) do
-        { e, v } = fraction_to_exponent(v, precision)
-        m = fraction_to_mantissa(v, 0, precision)
+    @spec integer_to_float(integer, integer, integer, rounding, encoding) :: { integer, integer }
+    defp integer_to_float(v, 0, precision, rounding, { _, ep, mp }) do
+        { e, v } = fraction_to_exponent(v, precision, Bit.set(ep - 1) - 1)
+        m = fraction_to_mantissa(v, 0, precision, rounding, mp)
         { ~~~e, m }
     end
-    defp integer_to_float(v, i, precision) do
-        { e, m } = whole_to_float(i)
-        m = fraction_to_mantissa(v, e, precision, m)
+    defp integer_to_float(v, i, precision, rounding, { _, _, mp }) do
+        { e, m } = whole_to_float(i, mp)
+        m = fraction_to_mantissa(v, e, precision, rounding, mp, m)
         { e, m }
     end
 
